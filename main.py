6d6550ea0d1f0ed6a382819812f040386e5a1abc
mparakouu <geobarakou7@gmail.com>
2024-07-08 23:29:46 +0300

    1) done

--- main.py
+++ main.py
@@ -1,34 +1,44 @@
-import torch
-from torchvision import datasets, transforms
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
+import torch
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
 
 
 def sample_of_classes():
-    # image --> tensor
-    # normalize pixel
     transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,))
+        transforms.ToTensor(), 
+        transforms.Normalize((0.5,), (0.5,))  
     ])
 
-    # MNIST dataset 
-    dataset = datasets.MNIST('../Thedata', train=True, download=True, transform=transform)
+    # load MNIST dataset
+    Dataset = datasets.MNIST(root='../Thedata', train=True, download=True, transform=transform)
 
-    samples = {}
+    # 10 classes --> 0 to 9
+    classes = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-    for image, label in dataset:
-        if label not in samples:
-            samples[label] = image
-        if len(samples) == 10:  
-            break
 
-    fig, axes = plt.subplots(1, 10, figsize=(15, 1.5))
-    for i in range(10):
-        axes[i].imshow(samples[i].squeeze(), cmap='gray')
-        axes[i].set_title(f'{i}')
-        axes[i].axis('off')
+    plt.figure(figsize=(10, 5))
+    for k in range(len(classes)):
+        plt.subplot(2, 5, k + 1)
+        sample_image = Dataset.data[Dataset.targets == k][0]
+        plt.imshow(sample_image, cmap='gray')
+        plt.title(f'Class {classes[k]}')
+        plt.axis('off')
 
+    plt.tight_layout()
     plt.show()
 
 
-sample_of_classes()
+def split_train_val(dataset, val_split=0.2):
+    train_size = int((1 - val_split) * len(dataset))
+    val_size = len(dataset) - train_size
+    train_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, val_size])
+    return train_dataset, val_dataset
+
+if __name__ == "__main__":
+
+    sample_of_classes()
+    
